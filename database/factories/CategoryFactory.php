755f2b8f3c91dd2f4b86a885732763bd479fe4b2
moinickcres <moinickcres@gmail.com>
2024-11-28 20:11:21 +0100

    CI/CD

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word, // Generates a random single word
+            'description' => $this->faker->sentence, // Generates a random sentence
+        ];
+    }
+}
